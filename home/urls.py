f1942ec76ff31b9456fdd165869ffa7f37cb067d
koiribrother <koiribrother123@gmail.com>
2022-12-01 22:19:24 +0545

    first commit

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from operator import index
+
+from .views import *
+from django.urls import path
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services')
+]
